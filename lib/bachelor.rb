b32c2b3a880df781901a351162b5ba570b564c03
Shawn Ren <doggy-ren@hotmail.com>
2019-03-21 16:18:05 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,6 +10,11 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
+  data.each do |season, season_array|
+    season_array.each do |contestant_hash|
+      contestant_hash.each do |attribute, value|
+        if attribute == "occupation" && value == occupation
+          return contestant_hash["name"]
   # code here
 end
 
